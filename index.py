dd38ac75bf78c55276ac1e973bccc10a8976d53e
yvjawahar <yvjawahar1003@gmail.com>
2024-02-16 20:38:48 +0530

    created index.py(frontend) and leetscraper.py(main code)

---
+++ index.py
@@ -0,0 +1,85 @@
+import streamlit as st
+import matplotlib.pyplot as plt
+import sys
+import json
+import time
+import pandas as pd
+
+# Add the path to the directory containing your scraping script
+sys.path.append("D:/LEET ANALYZER")
+
+from leetscraper import scrape_leetcode_profile
+
+# Streamlit UI
+st.set_page_config(layout='wide')
+color1 = '#1f77b4'  # blue
+color2 = '#ff7f0e'  # orange
+
+st.title("LeetCode **Profile** Analyzer")
+st.write("Compare..Evalute..Improve...")
+st.sidebar.header("Enter LeetCode Handles")
+#Input for first username
+st.sidebar.header("User 1")
+profile1 = st.sidebar.text_input("Enter first username:")
+
+# Input for second username
+st.sidebar.header("User 2")
+profile2 = st.sidebar.text_input("Enter second username:")
+# Function to display profile details
+def display_profile_details(profile_data, title):
+    st.write(f"### {title}")
+    st.write("---")
+    with st.container():
+        st.write(f"**Global Ranking:** {profile_data['global_ranking']}")
+        st.write(f"**Contests Attended:** {profile_data['contest_attended']}")
+        st.write(f"**Contest Rating:** {profile_data['contest_rating']}")
+        st.write(f"**Total Problems Solved:** {profile_data['total_problems_solved']}")
+        st.write(f"**Total Problems Submitted:** {profile_data['total_problems']}")
+        st.write("#### Problems Solved by Difficulty:")
+        st.write(f"- Easy: {profile_data['easy_solved']} / {profile_data['easy_total']}")
+        st.write(f"- Medium: {profile_data['medium_solved']} / {profile_data['medium_total']}")
+        st.write(f"- Hard: {profile_data['hard_solved']} / {profile_data['hard_total']}")
+def draw_circle_meter(problems_solved,total_problems, title):
+    # fig, ax = plt.subplots(figsize=(2,2))
+    # sizes = [problems_solved, total_problems - problems_solved]
+    # labels = [f"Solved: {problems_solved}", f"Remaining: {total_problems - problems_solved}"]
+    # colors = ['red', 'gray']
+    # ax.pie(sizes, labels=labels, colors=colors, startangle=90, counterclock=False, wedgeprops=dict(width=0.1))
+    # ax.axis('equal')
+    # ax.set_title(title, loc='center', fontsize=10, pad=20)
+    fixed_total_problems = 1000  # Set a fixed value for total problems
+    fixed_problems_solved = int(problems_solved / total_problems * fixed_total_problems)
+    
+    fig, ax = plt.subplots(figsize=(5, 5))
+    sizes = [fixed_problems_solved, fixed_total_problems - fixed_problems_solved]
+    labels = [f"Solved: {problems_solved}", f"Remaining: {total_problems - problems_solved}"]
+    colors = ['#1f77b4', '#dddddd']
+    ax.pie(sizes, labels=labels, colors=colors, startangle=90, counterclock=False, wedgeprops=dict(width=0.3))
+    ax.axis('equal')
+    ax.set_title(title, loc='left', fontsize=14, pad=20)
+    fig.patch.set_facecolor('#A7E45D')
+    st.pyplot(fig)
+
+if st.sidebar.button("Compare"):
+    if profile1 and profile2:
+        profile_data1 = scrape_leetcode_profile(profile1)
+        profile_data2 = scrape_leetcode_profile(profile2)
+        
+        if profile_data1 and profile_data2:
+            col1, col2 = st.columns(2)
+            with col1:
+                display_profile_details(profile_data1, profile_data1['name'])
+                draw_circle_meter(profile_data1['easy_solved'], profile_data1['easy_total'], "Easy Problems")
+                draw_circle_meter(profile_data1['medium_solved'], profile_data1['medium_total'], "Medium Problems")
+                draw_circle_meter(profile_data1['hard_solved'], profile_data1['hard_total'], "Hard Problems")
+
+            # Display profile details for user 2
+            with col2:
+                display_profile_details(profile_data2, profile_data2['name'])
+                draw_circle_meter(profile_data2['easy_solved'], profile_data2['easy_total'], "Easy Problems")
+                draw_circle_meter(profile_data2['medium_solved'], profile_data2['medium_total'], "Medium Problems")
+                draw_circle_meter(profile_data2['hard_solved'], profile_data2['hard_total'], "Hard Problems")
+        else:
+            st.write("Failed to fetch data for one or both usernames.")
+    else:
+        st.write("Please enter both usernames.")
---
+++ leetscraper.py
@@ -0,0 +1,138 @@
+import requests
+from bs4 import BeautifulSoup
+import re
+import json
+# def get_number_of_contest(number_of_contest):
+#     raw_text = number_of_contest.get_text(strip=True)
+#     print(raw_text)
+#     numeric_part = ''.join(c for c in raw_text if c.isdigit() or c == ',')
+#     numeric_value = int(numeric_part.replace(',', ''))
+#     return numeric_value
+def get_contest_rating(contest_span):
+    raw_text = contest_span.get_text(strip=True)
+    numeric_part = ''.join(c for c in raw_text if c.isdigit() or c == ',')
+    numeric_value = int(numeric_part.replace(',', ''))
+    return numeric_value
+
+def get_name(name_element):
+    if name_element:
+       name = name_element.get_text(strip=True)
+       return name
+    return "null"
+def get_rank(rank_span):
+    raw_text = rank_span.get_text(strip=True)
+    numeric_part = ''.join(c for c in raw_text if c.isdigit() or c == ',')
+    numeric_value = int(numeric_part.replace(',', ''))
+    return numeric_value
+
+def scrape_leetcode_profile(username):
+    url = f'https://leetcode.com/{username}/'
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        soup = BeautifulSoup(response.text, 'html.parser')
+
+        # Extracting details
+        name_element = soup.find('div', class_='text-label-1 dark:text-dark-label-1 break-all text-base font-semibold')
+        name=get_name(name_element)
+        rank= soup.select_one('span.ttext-label-1.dark\\:text-dark-label-1.font-medium')
+        rank=get_rank(rank)
+        contest_rating = soup.find('div','text-label-1 dark:text-dark-label-1 flex items-center text-2xl')
+        contest_rating=get_contest_rating(contest_rating)
+
+        contest = soup.find_all(class_='text-label-1 dark:text-dark-label-1 font-medium leading-[22px]')
+        global_rank=contest[0].get_text()
+        number_of_contest=int(contest[1].get_text())
+        
+        
+        easy=""
+        easy_solved=0
+        easy_total=0
+        medium=""
+        medium_solved=0
+        medium_total=0
+        hard=""
+        hard_solved=0
+        hard_total=0
+        total_problems_solved=0
+        total_problems=0
+        data=[]
+        problem_sections = soup.find_all('div', class_='space-y-2')
+        for section in problem_sections:
+            # Extract information from each section
+            problem_type_element = section.find('div', class_='w-[53px] text-label-3 dark:text-dark-label-3')
+            problem_solved_element = section.find('span', class_='mr-[5px] text-base font-medium leading-[20px] text-label-1 dark:text-dark-label-1')
+            problem_total_element = section.find('span', class_='text-xs font-medium text-label-4 dark:text-dark-label-4')
+            
+            # Check if the elements are found before calling get_text()
+            problem_type = problem_type_element.get_text(strip=True) if problem_type_element else None
+            problem_solved = problem_solved_element.get_text(strip=True) if problem_solved_element else None
+            problem_total = problem_total_element.get_text(strip=True) if problem_total_element else None
+
+            # Append the extracted information to the data list
+            data.append({
+                'problem_type': problem_type,
+                'problem_solved': problem_solved,
+                'problem_total': problem_total
+            })
+        for index in range(len(data)):
+            for key in data[index]:
+                if index==0:
+                    if key=='problem_type':
+                        easy=data[index][key]
+                    if key=='problem_solved':
+                        easy_solved=int(data[index][key])
+                        total_problems_solved+=easy_solved
+                    if key=='problem_total':
+                        easy_total=int(data[index][key][1:])
+                        total_problems+=easy_total
+                elif index==1:
+                    if key=='problem_type':
+                        medium=data[index][key]
+                    if key=='problem_solved':
+                        medium_solved=int(data[index][key])
+                        total_problems_solved+=medium_solved
+                    if key=='problem_total':
+                        medium_total=int(data[index][key][1:])
+                        total_problems+=medium_total
+                elif index==2:
+                    if key=='problem_type':
+                        hard=data[index][key]
+                    if key=='problem_solved':
+                        hard_solved=int(data[index][key])
+                        total_problems_solved+=hard_solved
+                    if key=='problem_total':
+                        hard_total=int(data[index][key][1:])
+                        total_problems+=hard_total
+
+        
+
+        return {
+            'name': name,
+            'rank': rank,
+            'contest_rating': contest_rating,
+            #'contests_attended': number_of_contest,
+            'easy': easy,
+            'easy_solved':easy_solved,
+            'easy_total':easy_total,
+            'medium':medium,
+            'medium_solved':medium_solved,
+            'medium_total':medium_total,
+            'hard':hard,
+            'hard_solved':hard_solved,
+            'hard_total':hard_total,
+            'total_problems_solved':total_problems_solved,
+            'total_problems':total_problems,
+            'global_ranking':global_rank,
+            'contest_attended':number_of_contest,
+        }
+
+    else:
+        print(f"Failed to fetch data for {username}. Status code: {response.status_code}")
+        return None
+
+profile_data = scrape_leetcode_profile(input())
+
+if profile_data:
+    json_data = json.dumps(profile_data, ensure_ascii=False).replace("'", '"')
+    print(profile_data)
